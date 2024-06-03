2039b1b8dc75677f5a00fe660799915310e43f07
prince6075 <princekumar9955888@gmail.com>
2024-06-04 01:48:10 +0530

    data ingestion

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ from sensor.exception import SensorException
 import sys
 import os
 from sensor.logger import logging
+from sensor.utilis import dump_csv_file_to_mongodb
 
-def test_exception():
-    try:
-        logging.info("error occur here diveded by zero")
-        a=1/0
-    except Exception as e:
-        raise SensorException(e,sys)
+
+
+# def test_exception():
+#     try:
+#         logging.info("error occur here diveded by zero")
+#         a=1/0
+#     except Exception as e:
+#         raise SensorException(e,sys)
 
 
 
 
 if __name__ == "__main__":
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
+    file_path = "C:/Users/princ/ML Project/LIvesensor/aps_failure_training_set1.csv"
+    database_name = "ineuron"
+    collection_name = "sensor"
+    dump_csv_file_to_mongodb(file_path,database_name,collection_name)
+
+
+
+
+    # try:
+    #     test_exception()
+    # except Exception as e:
+    #     print(e)
---
+++ sensor/entity/artificat_entity.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+
+class DataIngestionArtificat:
+    trained_file_path : str
+    test_file_path: str
+
